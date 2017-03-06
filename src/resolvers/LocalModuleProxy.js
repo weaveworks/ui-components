753d6618fbbeed692ad8d468e50cb10908633a2c
jpellizzari <jordan@weave.works>
2017-03-06 13:48:03 -0800

    Added option to proxy a list of local components

--- src/resolvers/LocalModuleProxy.js
+++ src/resolvers/LocalModuleProxy.js
@@ -6,15 +6,29 @@ module.exports = class LocalModuleProxy {
   apply(resolver) {
     const self = this;
     resolver.plugin('module', function (request, callback) {
-      const { moduleName, path } = self.opts;
-      if (self.opts.enabled && moduleName === request.request) {
-        const proxyReq = {
-          path: request.path,
-          request: path,
-          query: request.query,
-          directory: request.directory,
-        };
-        this.doResolve(['file'], proxyReq, callback);
+      const { moduleName, path, modules } = self.opts;
+      const requestedModule = request.request;
+
+      if (self.opts.enabled) {
+        if (moduleName === requestedModule) {
+          const proxyReq = {
+            path: request.path,
+            request: path,
+            query: request.query,
+            directory: request.directory,
+          };
+          this.doResolve(['file'], proxyReq, callback);
+        } else if (modules && modules[requestedModule]) {
+          const proxyReq = {
+            path: request.path,
+            request: modules[requestedModule],
+            query: request.query,
+            directory: request.directory,
+          };
+          this.doResolve(['file'], proxyReq, callback);
+        } else {
+          callback();
+        }
       } else {
         callback();
       }
