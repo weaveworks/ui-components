6c31acb3350c6691d13955b49e19481968c39c17
Filip Barl <filip.barl@gmail.com>
2018-07-20 17:04:46 +0200

    Extend Dialog component props.

--- src/components/Dialog/Dialog.test.js
+++ src/components/Dialog/Dialog.test.js
@@ -7,17 +7,17 @@ describe('<Dialog />', () => {
   it('shows/hides when active/inactive', () => {
     const closeSpy = jest.fn();
     const dialog = shallow(<Dialog active onClose={closeSpy} />);
-    expect(dialog.find('.weave-dialog').hasClass('active')).toBe(true);
-    dialog.find('.weave-dialog-close > span').simulate('click');
+    expect(dialog.get(0).props.active).toBe(true);
+    dialog.find({ text: '' }).simulate('click');
     expect(closeSpy).toBeCalled();
     dialog.setProps({ active: false });
-    expect(dialog.find('.weave-dialog').hasClass('active')).toBe(false);
+    expect(dialog.get(0).props.active).toBe(false);
   });
 
   it('runs the action-click handler', () => {
     const actionSpy = jest.fn();
     const dialog = shallow(
-      <Dialog actions={['Submit', 'Cancel']} onActionClick={actionSpy} />
+      <Dialog active actions={['Submit', 'Cancel']} onActionClick={actionSpy} />
     );
     dialog.find({ text: 'Submit' }).simulate('click');
     expect(actionSpy).toBeCalledWith('Submit');
@@ -28,7 +28,10 @@ describe('<Dialog />', () => {
   it('allows for React elements to be used for actions', () => {
     const newSpy = jest.fn();
     const dialog = shallow(
-      <Dialog actions={[<div className="user-action" onClick={newSpy} />]} />
+      <Dialog
+        active
+        actions={[<div className="user-action" onClick={newSpy} />]}
+      />
     );
     dialog.find('.user-action').simulate('click');
     expect(newSpy).toBeCalled();
