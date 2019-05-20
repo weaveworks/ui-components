6017018f832c9faa3c20811e155f500abda5ac3e
Filip Barl <filip.barl@gmail.com>
2019-05-20 12:53:02 +0200

    fix(shape): don't apply strokeDasharray on dottedtriangle highlight

--- src/components/GraphNode/shapes/_ShapeDottedTriangle.js
+++ src/components/GraphNode/shapes/_ShapeDottedTriangle.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import Shape, { curvedUnitPolygonPath } from './_Shape';
 
-const renderTemplate = attrs => (
-  <path d={curvedUnitPolygonPath(3)} strokeDasharray="0.1, 0.05" {...attrs} />
-);
+const renderTemplate = (attrs, { allowStroke = true } = {}) =>
+  allowStroke ? (
+    <path d={curvedUnitPolygonPath(3)} strokeDasharray="0.1, 0.05" {...attrs} />
+  ) : (
+    <path d={curvedUnitPolygonPath(3)} {...attrs} />
+  );
 
 export default class ShapeDottedTriangle extends React.Component {
   render() {
