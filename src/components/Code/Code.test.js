375cd9abcfac5903da10f18a05e604e76a8b3697
Simon Howe <footless@gmail.com>
2021-03-20 09:22:44 +0100

    fix: update Code.test.js tests
    - To use react-test-renderer like the other tests
    - Was a bit tricky to make shallow + withTheme play nice

--- src/components/Code/Code.test.js
+++ src/components/Code/Code.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import { withTheme } from '../../utils/theme';
 
@@ -7,30 +8,18 @@ import { CopyNotice } from './Code';
 
 import Code from '.';
 
+const getSnapShot = cmp => renderer.create(withTheme(cmp)).toJSON();
+
 document.execCommand = () => {};
 document.getSelection = () => ({
   toString: () => '',
 });
 
 describe('<Code />', () => {
-  let props;
-  let wrapper;
-  let onCopy;
-
-  beforeEach(() => {
-    onCopy = jest.fn();
-    props = {
-      onCopy,
-    };
-    wrapper = shallow(
-      <Code {...props}>such code much programming</Code>,
-      withTheme
-    );
-  });
-
   it('should call onCopy when the button is clicked', () => {
-    wrapper = mount(
-      withTheme(<Code {...props}>such code much programming</Code>)
+    const onCopy = jest.fn();
+    const wrapper = mount(
+      withTheme(<Code onCopy={onCopy}>such code much programming</Code>)
     );
     expect(onCopy).not.toHaveBeenCalled();
     wrapper.find(CopyNotice).simulate('click');
@@ -38,39 +27,38 @@ describe('<Code />', () => {
   });
 
   it('should render correctly', () => {
-    expect(wrapper.dive()).toMatchSnapshot();
+    const snapshot = getSnapShot(<Code>such code much programming</Code>);
+    expect(snapshot).toMatchSnapshot();
   });
 
   it('should render multi line string correctly with multiCommand=true', () => {
-    wrapper = shallow(
+    const snapshot = getSnapShot(
       <Code multiCommand>
         {`
 sudo curl -l git.io/scope -o /usr/local/bin/scope
 sudo chmod a+x /usr/local/bin/scope
 scope launch https://yjsjsubdx1h8un1f858gp7to8d51zdre@frontend.dev.weave.works
           `}
-      </Code>,
-      withTheme
+      </Code>
     );
-    expect(wrapper.dive()).toMatchSnapshot();
+    expect(snapshot).toMatchSnapshot();
   });
 
   it('should render multi line function correctly with multiCommand=true', () => {
-    wrapper = shallow(
+    const snapshot = getSnapShot(
       <Code multiCommand>
         {() => `
 sudo curl -l git.io/scope -o /usr/local/bin/scope
 sudo chmod a+x /usr/local/bin/scope
 scope launch https://yjsjsubdx1h8un1f858gp7to8d51zdre@frontend.dev.weave.works
       `}
-      </Code>,
-      withTheme
+      </Code>
     );
-    expect(wrapper.dive()).toMatchSnapshot();
+    expect(snapshot).toMatchSnapshot();
   });
 
   it('should render multi line jsx correctly with multiCommand=true', () => {
-    wrapper = shallow(
+    const snapshot = getSnapShot(
       <Code multiCommand>
         <div>sudo curl -L git.io/scope -o /usr/local/bin/scope</div>
         <div>sudo chmod a+x /usr/local/bin/scope</div>
@@ -78,9 +66,8 @@ scope launch https://yjsjsubdx1h8un1f858gp7to8d51zdre@frontend.dev.weave.works
           scope launch <wbr />
           https://yjsjsubdx1h8un1f858gp7to8d51zdre@frontend.dev.weave.works
         </div>
-      </Code>,
-      withTheme
+      </Code>
     );
-    expect(wrapper.dive()).toMatchSnapshot();
+    expect(snapshot).toMatchSnapshot();
   });
 });
